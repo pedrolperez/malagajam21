a27ee0ce3f1857649b3ccaa25fbc350e7e400dbf
Pfenei <adrsanrod2@gmail.com>
2021-06-11 02:32:52 +0100

    IC

--- Assets/Scripts/Knockback.cs
+++ Assets/Scripts/Knockback.cs
@@ -12,7 +12,7 @@ public class Knockback : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Player"))
         {
-            Rigidbody2D hit = collision.GetComponent<Rigidbody2D>();
+            var hit = collision.GetComponent<Rigidbody2D>();
             if (hit != null)
             {
                 Vector2 difference = hit.transform.position - transform.position;
@@ -23,19 +23,14 @@ public class Knockback : MonoBehaviour
                     hit.GetComponent<Enemy>().currentState = EnemyState.stagger;
                     collision.GetComponent<Enemy>().Knock(hit, knockTime, damage);
                 }
+
                 if (collision.gameObject.CompareTag("Player"))
-                {
                     if (collision.GetComponent<PlayerMovement>().currentState != PlayerState.stagger)
                     {
                         hit.GetComponent<PlayerMovement>().currentState = PlayerState.stagger;
                         collision.GetComponent<PlayerMovement>().Knock(knockTime, damage);
                     }
-
-                }
-
             }
         }
     }
-
 }
-
